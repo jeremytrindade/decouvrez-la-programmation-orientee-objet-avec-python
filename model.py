34dcb5431e9c2a693d2f94b99533ba9be68caee2
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-07-20 00:41:53 +0200

    C

--- model.py
+++ model.py
@@ -1,17 +1,32 @@
 import json
+import math
 class Agent:
     
     def say_hello(self, first_name):
         return "Bien le bonjour " + first_name + " !"
     
-    def __init__(self, **agent_attributes):
+    def __init__(self, position, **agent_attributes):
+        self.position = position
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
+
+class Position:
+    def __init__(self, longitude_degrees, latitude_degrees):
+        self.latitude_degrees = latitude_degrees
+        self.longitude_degrees = longitude_degrees
+
+    @property
+    def longitude(self):
+        # return 6 # pour voir si ça fonction
+        return self.longitude_degrees * math.pi /180
 
 def main():
     
     for agent_attributes in json.load(open("agents-100k.json")):
-        agent = Agent(**agent_attributes)
-        print(agent.agreeableness)
+        latitude = agent_attributes.pop('latitude')
+        longitude = agent_attributes.pop('longitude')
+        position = Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.longitude)
 
 main()
