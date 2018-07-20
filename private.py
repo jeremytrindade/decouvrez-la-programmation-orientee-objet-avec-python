2df87a0aa32ee435eb795457abca508b6047a74b
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-07-20 18:48:14 +0200

    Encapsulation pour comprendre les private (Comment fonctionne une machine a café)

---
+++ private.py
@@ -0,0 +1,12 @@
+class CoffeeMachine:
+
+    def start_machine(self):
+        
+        # start the machine
+
+    def boil_water(self):
+        # boils the water!
+
+    def make_coffee(self):
+        # yum, a good coffee!
+
