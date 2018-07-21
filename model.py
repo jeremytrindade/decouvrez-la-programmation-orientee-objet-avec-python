4d0f24752bf11afe9eedb1aa5c32cf5c72ec9b31
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-07-21 17:03:54 +0200

    Un probleme avec le module MATPLOTLIB

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 import json
 import math
 
-import matplotlib as mil # Pour mac
-mil.use('TkAgg') # pour mac
+import matplotlib as mil
+mil.use('TkAgg')
 import matplotlib.pyplot as plt
 
 class Agent:
@@ -136,8 +136,8 @@ class AgreeablenessGraph(BaseGraph):
         self.y_label = "agreeableness"
 
     def xy_values(self, zones):
-        x_values [zone.population_density() for zone in zones]
-        x_values [zone.average_agreeableness() for zone in zones]
+        x_values = [zone.population_density() for zone in zones]
+        x_values = [zone.average_agreeableness() for zone in zones]
         return x_values, y_values
         
 
